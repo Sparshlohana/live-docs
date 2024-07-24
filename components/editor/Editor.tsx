74cdddac0bff8c6cbbe6a1da36d193d1807c9651
Nikunj <sparsh.lohana@gmail.com>
2024-07-25 00:11:52 +0530

    comments added

--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -14,6 +14,7 @@ import FloatingToolbarPlugin from './plugins/FloatingToolbar';
 import React from 'react';
 import Loader from '../Loader';
 import { useThreads } from '@liveblocks/react/suspense';
+import Comments from '../Comments';
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -66,7 +67,7 @@ export function Editor({ roomId, currentUserType }: { roomId: string, currentUse
           <LiveblocksPlugin>
             <FloatingComposer className="w-[350px]" />
             <FloatingThreads threads={threads} />
-            {/* <Comments /> */}
+            <Comments />
           </LiveblocksPlugin>
         </div>
       </div>
