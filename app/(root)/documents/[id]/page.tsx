4ca17e456653f22ca9d4d18b2923b71e17f29ff9
Nikunj <sparsh.lohana@gmail.com>
2024-07-24 14:23:20 +0530

    added active colaborator

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,9 +1,29 @@
 import Room from "@/components/Room"
+import { getDocument } from "@/lib/actions/room.actions";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
 
-const Page = () => {
+const Page = async ({ params: { id } }: SearchParamProps) => {
+    const clerkUser = await currentUser();
+
+    if (!clerkUser) {
+        redirect('/sign-in');
+    }
+
+    const room = await getDocument({
+        roomId: id,
+        userId: clerkUser.emailAddresses[0].emailAddress,
+    });
+
+
+
+    // if (!room) {
+    //     redirect('/');
+    // }
+
     return (
         <main className="flex w-full flex-col items-center">
-           <Room/>
+            <Room roomId={id} roomMetadata={room?.metadata} />
         </main>
     )
 }
--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Loader from "@/components/Loader";
+import { getClerkUsers } from "@/lib/actions/user.actions";
 import {
     LiveblocksProvider,
     RoomProvider,
@@ -10,8 +11,11 @@ import { ReactNode } from "react";
 
 const Provider = ({ children }: { children: ReactNode }) => {
     return (
-        <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"}>
-            <ClientSideSuspense fallback={<Loader/>}>
+        <LiveblocksProvider authEndpoint={"/api/liveblocks-auth"} resolveUsers={async ({ userIds }) => {
+            const users = await getClerkUsers({userIds})
+            return users;
+        }}>
+            <ClientSideSuspense fallback={<Loader />}>
                 {children}
             </ClientSideSuspense>
         </LiveblocksProvider>
