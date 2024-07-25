0208ed1c378eb30bb7f9d79bfc03bf46a7e60102
Nikunj <sparsh.lohana@gmail.com>
2024-07-25 17:13:20 +0530

    live fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    typescript: {
+        ignoreBuildErrors: true,
+    },
     images: {
-        remotePatterns: [{protocol: 'https', hostname: 'img.clerk.com'}],
+        remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }],
     }
 };
 
